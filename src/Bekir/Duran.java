4edb867f2cad268fafde5b18d89688557f138166
Bekir Karalar <bekirdurankaralar>
2020-06-16 21:53:23 -0400

    second commit

--- src/Bekir/Duran.java
+++ src/Bekir/Duran.java
@@ -3,5 +3,7 @@ package Bekir;
 public class Duran {
     public static void main(String[] args) {
         System.out.println("Karalar");
+        System.out.println("Bekir");
+        System.out.println("Duran");
     }
 }
